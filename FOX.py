e8cf7e45ce478620f1a5575baf32cec7e7eb7a63
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-11-25 19:00:27 +0100

    .

--- FOX.py
+++ FOX.py
@@ -10,21 +10,21 @@ try:
     if sys.argv[1]=='update':
         os.system('rm -rf FOX.so FOX32.so')
 except:
-    pass 
+    pass
 os.system('rm -rf FOX.so FOX32.so')
 os.system('git pull')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('FOX.so'):
-        os.system('curl -L https://github.com/chigoziieworldwide/foxx/blob/main/FOX.cpython-311.so?raw=true -o FOX.so') 
+    if not os.path.isfile('ROX.so'):
+        os.system('curl -L https://github.com/chigoziieworldwide/exee/blob/main/FOX.cpython-311.so?raw=true -o FOX.so') 
         __import__("FOX").chigozie()
     else:
         __import__("FOX").chigozie()
 
 elif bit == '32bit':
-    if not os.path.isfile('FOX32.so'):
-        os.system('curl -L https://github.com/chigoziieworldwide/foxx/blob/main/FOX32.cpython-311.so?raw=true -o FOX32.so') 
+    if not os.path.isfile('ROX32.so'):
+        os.system('curl -L https://github.com/chigoziieworldwide/exee/blob/main/FOX32.cpython-311.so?raw=true -o FOX32.so') 
         __import__("FOX32").chigozie()
     else:
         __import__("FOX32").chigozie()
